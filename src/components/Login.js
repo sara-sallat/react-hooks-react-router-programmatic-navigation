0adee3e7b5d0b03eea7dcd2c6b51dabe1c06d3af
sara sallat <sallatsara@gmail.com>
2023-01-17 20:06:00 +0300

    done

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,6 +3,7 @@ import { useHistory } from "react-router-dom";
 
 function Login({ setIsLoggedIn }) {
   const history = useHistory();
+  console.log(history);
   const [formData, setFormData] = useState({
     username: "",
     password: "",
